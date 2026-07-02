93284ccf9104abd05d6c124b0b1a1af337a9626b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-02 13:23:06 +0700

    update: add Call To Action section and footer

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,14 +1,18 @@
-import NavBar from "../components/navigations/NavBar"
+import NavBar from "../layouts/NavBar"
 import Hero from "../sections/Hero"
 import Features from "../sections/Features"
+import CallToAction from "../sections/CallToAction"
+import Footer from "../layouts/Footer"
 
 function Landing() {
     return (
     <div className={`w-full flex flex-col justify-center`}>
         <NavBar />
-        <div className={`max-w-480 w-full min-h-screen pb-40`}>
+        <div className={`max-w-480 w-full min-h-screen`}>
             <Hero />
             <Features />
+            <CallToAction />
+            <Footer />
         </div>
     </div>
 )}
