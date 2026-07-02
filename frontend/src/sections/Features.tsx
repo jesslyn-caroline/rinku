17edcddcd37f16ec38e672c06c77a7989ff09883
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-02 12:08:03 +0700

    fix: card fit on mobile and add border-b for third card on mobile view

--- frontend/src/sections/Features.tsx
+++ frontend/src/sections/Features.tsx
@@ -5,7 +5,7 @@ function Features() {
             <span className={`text-2xl font-medium`}>Features</span>
             <span className={`uppercase text-secondary font-dm-mono text-xs tracking-widest leading-5`}>What You Get</span>
         </h2>
-        <ul className={`grid grid-rows-4 md:grid-cols-2 md:grid-rows-2`}>
+        <ul className={`grid grid-cols-1 md:grid-cols-2 md:grid-rows-2`}>
             <li className={`flex flex-col gap-y-3 px-6 lg:px-12 py-10 border-b md:border-r border-text/12`}>
                 <h3 className={`text-lg font-medium`}>Instant URL Shortening</h3>
                 <p className={`text-secondary font-noto-sans-jp text-sm`}>Convert long URLs into clean, shareable links with just one click.</p>
@@ -14,7 +14,7 @@ function Features() {
                 <h3 className={`text-lg font-medium`}>Daily Click Analytics</h3>
                 <p className={`text-secondary font-noto-sans-jp text-sm`}>Monitor how many times your links are clicked each day with clear, easy-to-read charts that help you measure performance.</p>
             </li>
-            <li className={`flex flex-col gap-y-3 px-6 lg:px-12 py-10 md:border-r border-text/12`}>
+            <li className={`flex flex-col gap-y-3 px-6 lg:px-12 py-10 border-b md:border-b-0 md:border-r border-text/12`}>
                 <h3 className={`text-lg font-medium`}>Customize Your Links</h3>
                 <p className={`text-secondary font-noto-sans-jp text-sm`}>Create personalized aliases that match your brand or campaign.</p>
             </li>
