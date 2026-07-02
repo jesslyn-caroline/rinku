18a93367126f916e25308a1bcf98fab51a1372eb
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-02 13:29:50 +0700

    fix: change year

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
     <footer className={`w-full px-6 md:px-16 py-4`}>
-        <span className={`font-dm-mono text-xs text-secondary tracking-widest`}>© 2024 Rinku.io. All rights reserved.
-
+        <span className={`font-dm-mono text-xs text-secondary tracking-widest`}>
+            © 2026 Rinku.io. All rights reserved.
         </span>
     </footer>
 )}
