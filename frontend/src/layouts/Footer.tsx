d0f9138e46a6190ba2b054d3e456366950fdfd1d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-02 13:22:41 +0700

    new layout

---
+++ frontend/src/layouts/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+    <footer className={`w-full px-6 md:px-16 py-4`}>
+        <span className={`font-dm-mono text-xs text-secondary tracking-widest`}>© 2024 Rinku.io. All rights reserved.
+
+        </span>
+    </footer>
+)}
+
+export default Footer
