8e11bb9b50ff3ca41f976e9f45fb263313bd0ec2
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-31 12:31:35 +0700

    update: add layout structure

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,12 +1,35 @@
 import { Outlet } from "react-router-dom"
 import SideBar from "./Sidebar"
-
+import { RiMenuLine } from "@remixicon/react"
+import { useEffect, useState } from "react"
+import Logo from "../components/others/Logo"
 
 function DashboardLayout() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(true)
+    const toggleSideBar = (value: boolean) => setIsOpen(value)
+
+    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768)
+    const handleResize = () => setIsMobile(window.innerWidth < 768)
+
+    useEffect(() => window.addEventListener('resize', handleResize))
+
     return (
     <div className={`flex flex-row relative`}>
-        <SideBar />
-        <Outlet />
+        {/* Overlay */}
+        <div className={`absolute w-full min-h-screen h-full bg-black/40 z-10 ${!isMobile || !isOpen? 'hidden' : 'block'}`} />
+
+        <SideBar isOpen={isOpen} toggleSideBar={toggleSideBar} isMobile={isMobile} />
+
+        <div className={`w-full flex flex-col divide-y divide-text/12`}>
+            <div className={`flex flex-row justify-between items-center px-5 py-3`}>
+                <RiMenuLine size={20} onClick={() => toggleSideBar(true)} className={isOpen ? 'hidden' : 'block'}/>
+                <Logo />     
+            </div>
+            <div className={`p-4`}>
+                <Outlet />
+            </div>
+        </div>
     </div>
 )}
 
