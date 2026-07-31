4bb2244bfb1e9533f73de4e335eea87d65fa7a2c
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-07-31 12:31:57 +0700

    update: add responsiveness

--- frontend/src/layouts/Sidebar.tsx
+++ frontend/src/layouts/Sidebar.tsx
@@ -1,8 +1,14 @@
-import { RiBarChartFill, RiLink, RiLogoutBoxLine, RiNumbersFill, RiSettings3Line  } from "@remixicon/react"
+import { RiBarChartFill, RiLink, RiLogoutBoxLine, RiMenuFoldLine, RiNumbersFill, RiSettings3Line  } from "@remixicon/react"
 import { Link, useLocation } from "react-router-dom"
 import TextButton from "../components/buttons/TextButton"
 
-function SideBar() {
+interface Props {
+    isOpen: boolean,
+    isMobile: boolean,
+    toggleSideBar: (value: boolean) => void
+}
+
+function SideBar({ isOpen, isMobile, toggleSideBar }: Props) {
     const location = useLocation()
 
     const navigation = [
@@ -13,27 +19,31 @@ function SideBar() {
     ]
 
     return (
-    <div className={`max-w-64 bg-accent h-screen w-full grid grid-rows-[fit-content(48px)_1fr_fit-content(48px)] sticky top-0 border-r border-text/12 divide-y divide-text/12`}>
-        <div className={`flex flex-row gap-x-3 items-center px-5 py-3`}>
-            <span className={`size-6 bg-primary flex items-center justify-center`}>
-                <RiLink size={14} className={`text-white`}/>
-            </span>
-            <h1 className={`text-lg`}>rinku.io</h1>
+    <div className={`
+        ${ isOpen ? 'translate-x-0' : '-translate-x-full hidden' } ${ isMobile ? 'absolute' : 'sticky' } 
+        max-w-64 bg-accent h-screen w-full z-20 top-0
+        grid grid-rows-[fit-content(48px)_1fr_fit-content(48px)] 
+        border-r border-text/12 divide-y divide-text/12
+    `}>
+        <div className={`px-5 py-4`}>
+            <RiMenuFoldLine size={20} className={`text-secondary`} onClick={() => toggleSideBar(false)}/>       
         </div>
         <div className={`flex flex-col gap-y-1 px-3 py-4`}>
             { navigation.map((nav) => {
+                const { path, label, Icon } = nav
                 return (
                 <Link to={nav.path} className={`
-                    w-full h-fit p-3 flex flex-row items-center gap-x-4
-                    uppercase font-dm-mono text-sm tracking-widest
-                    ${ location.pathname === nav.path ? 
-                        'bg-primary/10 text-black font-medium border-l-3 border-primary' : 
-                        'text-secondary hover:bg-secondary/10'
-                    }
-                    transition
+                    outline-none w-full h-fit p-3 flex flex-row items-center gap-x-4 
+                    ${ location.pathname === path ? 
+                        'bg-primary/10 border-l-3 border-primary' : 
+                        'hover:bg-secondary/10' 
+                    } transition
                 `}>
-                    <nav.Icon size={18} className={location.pathname === nav.path ? 'text-primary' : 'text-secondary'}/>
-                    <span className={``}>{ nav.label }</span>
+                    <Icon size={18} className={location.pathname === nav.path ? 'text-primary' : 'text-secondary'}/>
+                    <span className={`
+                        uppercase font-dm-mono text-sm tracking-widest 
+                        ${location.pathname === nav.path ? 'text-text font-medium' : 'text-secondary'}
+                    `}>{ label }</span>
                 </Link>
             )}) }
         </div>
